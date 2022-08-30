72795eda497e4014d9267ec1b2b2aba378e7882d
raphhh2103 <35596159+raphhh2103@users.noreply.github.com>
2022-08-30 17:01:14 +0200

    début implémentation dbconnect

--- DbLabo/DbConnect.cs
+++ DbLabo/DbConnect.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DbLabo.DbEntities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +10,28 @@ namespace DbLabo
 {
     public class DbConnect :DbContext
     {
+        private readonly string _connectionString;
 
+        public DbSet<UserEntity> Users { get; set; }
 
+        public DbSet<SkillEntity> Skills { get; set; }
+
+        public DbSet<EquipmentEntity> Equipments { get; set; }
+
+        public DbSet<ChampEntity> Champs { get; set; }
+        public DbSet<BasicsStatisticEntity> BasicsStatistics { get; set; }
+
+        public DbSet<AffinityChampEntity> AffinityChamps { get; set; }
+
+        public DbConnect()
+        {
+            this._connectionString = @"";
+        }
+        public DbConnect(string connectionString):base()
+        {
+            this._connectionString=connectionString;
+        }
+    
+    
     }
 }
