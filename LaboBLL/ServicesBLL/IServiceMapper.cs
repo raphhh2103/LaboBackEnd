310c637b0a8395973176acdeef352c600ce3f2e8
raphhh2103 <35596159+raphhh2103@users.noreply.github.com>
2022-08-31 16:52:47 +0200

    end day

---
+++ LaboBLL/ServicesBLL/IServiceMapper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboBLL.ServicesBLL
+{
+    public interface IServiceMapper<Tmodels>
+    {
+        public Tmodels Create(Tmodels entity);
+        public Tmodels Update(Tmodels entity);
+        public Tmodels GetOne(string str);
+        public IEnumerable<Tmodels> GetAll();
+        public Tmodels Delete(string str);
+    }
+}
