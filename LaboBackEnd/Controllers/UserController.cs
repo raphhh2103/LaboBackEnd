834bf99198a3a1686fc946aed54805448b1d9483
raphaelk <raphaelk@live.be>
2022-08-31 23:10:34 +0200

    good night !

---
+++ LaboBackEnd/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using LaboBLL.ServicesBLL;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LaboBackEnd.Controllers
+{
+    [ApiController]
+    [Route("User")]
+    public class UserController : ControllerBase
+    {
+        private readonly UserSericeBLL _userServiceBLL;
+
+        public UserController(UserSericeBLL userSericeBLL)
+        {
+            this._userServiceBLL = userSericeBLL;
+        }
+      
+        [HttpPost]
+        public IActionResult UserCreating()
+        {
+            return Ok();
+        }
+
+    }
+}
